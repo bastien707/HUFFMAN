c6706bd57b9e0dc08f01451e715705c3ef4b8435
bastien707 <bastien.facqueur@efrei.net>
2020-11-18 10:08:13 +0100

    add temporary struct

--- include/huffman_tree.h
+++ include/huffman_tree.h
@@ -3,6 +3,13 @@
 #include "occurrence.h"
 #include "CharToBinary.h"
 
+typedef struct Node{
+    int freq;
+    char lettter;
+    struct Node* left;
+    struct Node* right;
+}Node;
+
 Element* min_list(Element *list); 
 void list_remove_element(Element **list, Element *min);
 
