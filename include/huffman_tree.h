a112726f2ad48c7c7c7d1d22d26633ed3b8b08c0
bastien707 <bastien.facqueur@efrei.net>
2020-11-18 14:55:40 +0100

    trying to debug huffman tree

--- include/huffman_tree.h
+++ include/huffman_tree.h
@@ -4,13 +4,32 @@
 #include "CharToBinary.h"
 
 typedef struct Node{
-    int freq;
-    char lettter;
+    int occ;
+    char letter;
     struct Node* left;
     struct Node* right;
 }Node;
 
-Element* min_list(Element *list); 
-void list_remove_element(Element **list, Element *min);
+typedef struct List_Node{
+    struct List_Node* next;
+    Node* data;
+}List_Node;
+
+List_Node* min_list_node(List_Node *list_node); 
+
+void list_remove_node(List_Node **list_node, List_Node *min_node);
+
+List_Node* convert_elem_to_Node(Element* list);
+
+Node* create_node_from_element(Element* element);
+
+List_Node* create_list_node_from_element(Element* list);
+
+Node* huffman_tree(List_Node* list_node);
+
+Node* create_Node(List_Node* min1, List_Node* min2);
+
+List_Node* create_list_node(Node* new_node);
+
 
 #endif
