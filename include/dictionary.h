03e6d64a686620c708541a4425aae0d31b0fe247
bastien707 <bastien.facqueur@efrei.net>
2020-11-25 13:40:02 +0100

    trying to create huffman_dictionary

---
+++ include/dictionary.h
@@ -0,0 +1,8 @@
+#ifndef dictionary
+
+#include "huffman_tree.h"
+
+/// return an array coded with huffman
+void huffman_dictionary(Node* tree, char **DictionaryArray, char array[], int index );
+
+#endif
