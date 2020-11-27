2d17fbf5395392de76e74a4a989db17cd85dbcfb
bastien707 <bastien.facqueur@efrei.net>
2020-11-27 19:23:46 +0100

    Doxygen documentation

---
+++ include/Dictionary.h
@@ -0,0 +1,38 @@
+/*************************************************************************
+ * \file   Dictionary.h
+ * \brief  Header of the part that create an huffman dictionary 
+ *      
+ * \date   November 2020
+ ***********************************************************************/
+#ifndef DICTIONARY
+#define DICTIONARY
+
+#include "huffman_tree.h"
+
+/**
+ * @brief create a dictionary from an huffman tree
+ * 
+ * @param tree a pointer to an huffman tree 
+ * @param array array that recover the path to a char 
+ * @param index index for the array
+ */
+extern void HuffmanDictionary(Node* tree, char array[], int index);
+
+/**
+ * @brief Write an array in the dictionary
+ * 
+ * @param array array that contain a path to the char
+ * @param size size of the array
+ * @param tree a pointer to a tree
+ */
+ extern void WriteArray(char array[], int size, Node* tree);
+
+/**
+ * @brief this function return the depth of any tree
+ * 
+ * @param tree a pointer to a tree
+ * @return depth of the tree
+ */
+extern unsigned int depth(Node* tree);
+
+#endif // !DICTIONARY
