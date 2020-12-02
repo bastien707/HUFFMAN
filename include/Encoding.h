4b4b57ee71ea4ec41e375dda90329aeda4f0ee00
bastien707 <bastien.facqueur@efrei.net>
2020-12-02 16:23:53 +0100

    Encode a file

---
+++ include/Encoding.h
@@ -0,0 +1,24 @@
+/*************************************************************************
+ * \file   Encoding.h
+ * \brief  Header that contain function who encode a text 
+ *      
+ * \date   November 2020
+ ***********************************************************************/
+#ifndef ENCODING
+#define ENCODING
+
+/**
+ * @brief Function to write a char into his binary huffman form
+ * 
+ * @param DIC the huffman dictionary
+ * @param c char want to convert 
+ */
+void write_huffman_text(FILE* DIC, char c);
+
+/**
+ * @brief Function to encode a whole text into huffman code
+ * 
+ */
+void encoding();
+
+#endif //!ENCODING
